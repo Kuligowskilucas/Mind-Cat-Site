04e325e7a6e5b88907a0bf2c99eb3ab70d646c17
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2024-05-26 20:21:47 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="darkreader" content="disable">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/padroes.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<header>
+    <div class="container-header">
+        <div class="conteudo-1998">
+            <div class="conteudo-header item-flex">
+                <div class="quadrado-branco item-flex">
+                    <img src="img/icone-gato.svg" alt="logo do gato" width="69px" height="61px">
+                </div>
+                <div class="botoes item-flex">
+                    <div class="botao-entrar">
+                        <a href=""><button>Entrar</button></a>
+                    </div>
+                    <div class="botao-criar">
+                        <a href=""><button>Criar</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<body>
+    <div class="container-imagens-celular">
+        <div class="conteudo-1300">
+            <div class="conteudo-imagens-celular item-flex">
+                <div class="escala-gatinho">
+                    <img src="img/escala_gatinho.png" alt="imagem da escala gatinho">
+                </div>
+                <div class="abertura-app">
+                    <img src="img/abertura_app.png" alt="imagem da abertura do app">
+                </div>
+                <div class="atividades">
+                    <img src="img/atividades.png" alt="imagem da tela de atividades">
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container-terapia">
+        <div class="conteudo-1200">
+            <div class="conteudo-terapia item-flex">
+                <div class="nota-texto item-flex">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
